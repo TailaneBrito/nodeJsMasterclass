427637f6108f5e3679336f710500d3b51c9dee1d
Brito, Tailane <tailane.brito01@gmail.com>
2021-02-04 23:49:14 -0300

    all about headers

--- Basics/dataResonse.js
+++ Basics/dataResonse.js
@@ -20,9 +20,15 @@ const data = [
 ]
 
 const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Language', 'en-US');
+    res.setHeader('Date', new Date(Date.now()));
+    res.setHeader('X-Powered-By', 'Node.js')
+
     res.end(
         JSON.stringify({ 
             success : true,
+            results : data.length,
             data : data
         })
     )
